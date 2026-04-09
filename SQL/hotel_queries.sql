3c2fa4b7f610eaa0aa7b62bfa614d0ee3c0e0c0c
Shivaleela-06 <shivaleelaanna650@gmail.com>
2026-04-09 12:16:17 +0530

    Update hotel_queries.sql

--- SQL/hotel_queries.sql
+++ SQL/hotel_queries.sql
@@ -6,3 +6,60 @@ JOIN (
     GROUP BY user_id
 ) lb
 ON b.user_id = lb.user_id AND b.booking_date = lb.last_booking;
+
+SELECT bc.booking_id,
+       SUM(i.item_rate * bc.item_quantity) AS total_bill
+FROM booking_commercials bc
+JOIN items i ON bc.item_id = i.item_id
+WHERE bc.bill_date BETWEEN '2021-11-01' AND '2021-11-30'
+GROUP BY bc.booking_id;  
+
+
+SELECT bc.bill_id,
+       SUM(i.item_rate * bc.item_quantity) AS bill_amount
+FROM booking_commercials bc
+JOIN items i ON bc.item_id = i.item_id
+WHERE bc.bill_date BETWEEN '2021-10-01' AND '2021-10-31'
+GROUP BY bc.bill_id
+HAVING SUM(i.item_rate * bc.item_quantity) > 1000; 
+
+
+WITH item_counts AS (
+    SELECT 
+        DATE_FORMAT(bc.bill_date, '%Y-%m') AS month,
+        bc.item_id,
+        SUM(bc.item_quantity) AS total_qty
+    FROM booking_commercials bc
+    WHERE YEAR(bc.bill_date) = 2021
+    GROUP BY month, bc.item_id
+),
+ranked AS (
+    SELECT *,
+           RANK() OVER (PARTITION BY month ORDER BY total_qty DESC) AS most_rank,
+           RANK() OVER (PARTITION BY month ORDER BY total_qty ASC) AS least_rank
+    FROM item_counts
+)
+SELECT *
+FROM ranked
+WHERE most_rank = 1 OR least_rank = 1; 
+
+
+WITH monthly_bills AS (
+    SELECT 
+        DATE_FORMAT(bc.bill_date, '%Y-%m') AS month,
+        b.user_id,
+        SUM(i.item_rate * bc.item_quantity) AS total_bill
+    FROM booking_commercials bc
+    JOIN bookings b ON bc.booking_id = b.booking_id
+    JOIN items i ON bc.item_id = i.item_id
+    WHERE YEAR(bc.bill_date) = 2021
+    GROUP BY month, b.user_id
+),
+ranked AS (
+    SELECT *,
+           DENSE_RANK() OVER (PARTITION BY month ORDER BY total_bill DESC) AS rnk
+    FROM monthly_bills
+)
+SELECT *
+FROM ranked
+WHERE rnk = 2;
